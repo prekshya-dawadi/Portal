66ec1dcee730866cd97a04aaee17e595bee9ad4c
Prekshya Dawadi <prekshya019@gmail.com>
2022-10-20 00:08:53 +0000

    All pages basic styling and structure

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Portal' });
 });
 
+router.get('/chatrooms', function(req, res, next){
+  res.render('chatrooms');
+});
+
+router.get('/contribute', function(req, res, next){
+  res.render('contribute');
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about');
+});
+
+router.get('/feedback', function(req, res, next){
+  res.render('feedback');
+});
+
 module.exports = router;

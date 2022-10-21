c1f9313c30e7bc702463d2da2e5c02851eae23f6
Prekshya Dawadi <prekshya019@gmail.com>
2022-10-21 10:57:33 +0000

    Image fixed

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.get('/feedback', function(req, res, next){
   res.render('feedback');
 });
 
-router.get('/chat-window/:id', async function(req, res, next){
+router.get('/chat/:id', async function(req, res, next){
   const channel = await cards.findOne({_id: req.params.id});
   console.log(channel);
   res.render('chat-window',{channel: channel});
